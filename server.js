c483047ffc24686ed359979b064bd0ce52c7e1dd
Isidora <korac.isidora@gmail.com>
2020-05-21 13:49:21 +0200

    heroku

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 const multer = require("multer");
 const config = require("config");
 const cors = require("cors");
+const path = require("path");
 
 const app = express();
 
@@ -31,6 +32,15 @@ mongoose
 app.use("/api/items", require("./routes/api/items"));
 app.use("/api/users", require("./routes/api/users"));
 app.use("/api/auth", require("./routes/api/auth"));
+
+// Serve static assets if in production
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 const port = process.env.PORT || 5000;
 
